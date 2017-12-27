e982a98f357500a9d552dd2adbcbf5c161dc5fbe
John Faithorn <john.faithorn@futurenet.com>
2017-12-27 15:57:08 -0800

    initial commit, bare react starter, no redux yet

---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component {
+    constructor(props) {
+      super(props);
+    }
+    render(){
+        return(
+            <h1>hi</h1>
+        )
+    }
+
+}
+
+export default Main
